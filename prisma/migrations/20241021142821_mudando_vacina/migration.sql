555ae508e93a66d6ebefd48fb6a7f0e75bc7094f
Fulano de Tal <gabriel.andrade200p@gmail.com>
2024-10-30 11:12:40 -0300

    tirando migrations

--- prisma/migrations/20241021142821_mudando_vacina/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `especie` to the `Vacina` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Vacina" ADD COLUMN     "especie" "TIPO_ANIMAL" NOT NULL;
